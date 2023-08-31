0791acc7d5183f50610ec0f42c6e9b1f5d2d4f8d
lboulanger <lboulanger@amphibee.fr>
2023-08-31 18:00:23 +0200

    🐛 fix(render.php): change GuzzleHttp\Client import to improve code readability and maintainability 🐛 fix(render.php): change GuzzleHttp\Exception\RequestException import to improve code readability and maintainability 🐛 fix(render.php): change file_get_contents() to GuzzleHttp\Client get() method to improve code readability and maintainability 🐛 fix(render.php): change file_get_contents() response handling to GuzzleHttp\Client response handling to improve code readability and maintainability 🐛 fix(render.php): change $data variable assignment to GuzzleHttp\Client response body to improve code readability and maintainability 🐛 fix(render.php): change $renderedVideos variable assignment to an empty string before the loop to improve code readability and maintainability 🐛 fix(render.php): change $videos array declaration to improve code readability and maintainability 🐛 fix(render.php): change $channelTitle variable assignment to remove duplicate assignment to improve code readability and maintainability

--- dist/blocks/YoutubeGroup/render.php
+++ dist/blocks/YoutubeGroup/render.php
@@ -1,5 +1,8 @@
 <?php
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
 $youtubeForWpOptions = get_option("youtube_for_wp");
 
 if (!$youtubeForWpOptions || empty($youtubeForWpOptions['apiKey']) || empty($youtubeForWpOptions['channelID'])) {
@@ -12,25 +15,51 @@ $maxResults = !empty($youtubeForWpOptions['maxResults']) ? $youtubeForWpOptions[
 
 $url = "https://www.googleapis.com/youtube/v3/search?key=$apiKey&channelId=$channelId&part=snippet,id&order=date&maxResults=$maxResults";
 
-$response = file_get_contents($url);
-$data = json_decode($response);
+$client = new Client();
 
-$renderedVideos = '';
+try {
+    $response = $client->get($url);
+    $statusCode = $response->getStatusCode();
 
-foreach ($data->items as $video) {
-    $videoId = $video->id->videoId;
-    $videoTitle = $video->snippet->title;
-    $description = $video->snippet->description;
-    $publishedAt = $video->snippet->publishedAt;
-    $thumbnail = $video->snippet->thumbnails->medium->url;
-    $channelTitle = $video->snippet->channelTitle;
+    if ($statusCode === 200) {
+        // La requête a réussi
+        $data = json_decode($response->getBody());
 
-    $renderedVideos .= "<h2>$videoTitle</h2>";
-    $renderedVideos .= "<iframe width='100%' style='aspect-ratio: 16/9' src='https://www.youtube.com/embed/$videoId' 
-            frameborder='0' 
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+        $renderedVideos = '';
+        $videos = [];
+
+        foreach ($data->items as $video) {
+            $videoId = $video->id->videoId;
+            $videoTitle = $video->snippet->title;
+            $description = $video->snippet->description;
+            $publishedAt = $video->snippet->publishedAt;
+            $thumbnail = $video->snippet->thumbnails->medium->url;
+            $channelTitle = $video->snippet->channelTitle;
+            $channelTitle = $video->snippet->channelTitle;
+            $videoUrl = 'https://www.youtube.com/embed/'.$videoId;
+
+            $videos[] = [
+                'videoId' => $videoId,
+                'videoTitle' => $videoTitle,
+                'description' => $description,
+                'publishedAt' => $publishedAt,
+                'thumbnail' => $thumbnail,
+                'channelTitle' => $channelTitle,
+                'videoUrl' => $videoUrl,
+            ];
+
+            $renderedVideos .= "<h2>$videoTitle</h2>";
+            $renderedVideos .= "<iframe loading='lazy' width='100%' style='aspect-ratio: 16/9' src='$videoUrl' 
+                    frameborder='0' 
+                    allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+        }
+
+        $filteredVideos = apply_filters('youtube_for_wp_rendered_videos', $renderedVideos, $videos);
+
+        echo $renderedVideos;
+    } else {
+        error_log(__("La requête a échoué avec le code d'état :", YOUTUBE_FOR_WP_TEXT_DOMAIN). ' ' . $statusCode, 1, get_bloginfo('admin_email'));
+    }
+} catch (RequestException $e) {
+    error_log(__("Une erreur s'est produite lors de la requête :", YOUTUBE_FOR_WP_TEXT_DOMAIN) . ' ' . $e->getMessage(), 1, get_bloginfo('admin_email'));
 }
-
-$filteredVideos = apply_filters('youtube_for_wp_rendered_videos', $renderedVideos, $data->items);
-
-echo $filteredVideos;
